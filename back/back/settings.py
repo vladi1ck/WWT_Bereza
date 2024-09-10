a84d87b293b658d79c07cc5c90248859571c20a2
vladi1ck <borisevich.vlad98@gmail.com>
2024-09-10 14:09:23 +0300

    minor fixes

--- back/back/settings.py
+++ back/back/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.RequestTimeMiddleware',
+    # 'core.middleware.RequestTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'back.urls'
